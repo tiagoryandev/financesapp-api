fcdd194dfbc36253a36f9c302b7a6b40ca15bc9e
TiaGoiNsaNy <tiagoinsany@gmail.com>
2022-03-18 09:40:24 -0300

    refactor: refector the code and add configurations in Jest

--- global.d.ts
+++ global.d.ts
@@ -3,6 +3,7 @@ export declare global {
         interface ProcessEnv extends EnvConfig {
             NODE_ENV: "development" | "production";
             DATABASE_URL: string;
+            PORT: number;
         }
     }
 }
